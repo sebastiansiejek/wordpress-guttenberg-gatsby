a15d5f7ff9a8374b30ba394e37bc1cef58af34e5
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-08-23 15:01:29 +0200

    feat: add pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,34 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(
+    `
+      query {
+        wpgraphql {
+          pages {
+            edges {
+              node {
+                id
+                title
+                slug
+                date
+                content
+              }
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) throw result.errors
+    result.data.wpgraphql.pages.edges.forEach(({ node }) => {
+      createPage({
+        path: `/${node.slug}`,
+        component: path.resolve('./src/templates/Page/Page.jsx'),
+        context: node,
+      })
+    })
+  })
+}
