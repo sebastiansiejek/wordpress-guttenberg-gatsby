4cc7d5c33ee21b8ac90cbb4a31037f118b74b863
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-08-27 19:49:17 +0200

    chore: add sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,8 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-postcss`,
     {
       resolve: 'gatsby-plugin-root-import',
       options: {
